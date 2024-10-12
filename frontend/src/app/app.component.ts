27d9aa6765a3f8655ffbcb68cf59ba9215c0d5e0
jkarthaus <Joern.karthaus@gmail.com>
2024-10-12 16:19:32 +0200

    implement accordeon

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,26 +1,30 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {FullCalendarModule} from "@fullcalendar/angular";
-import {CalendarOptions} from "@fullcalendar/core";
-import multiMonthPlugin from '@fullcalendar/multimonth';
 import {EnquiryFormComponent} from "./enquiry-form/enquiry-form.component";
 import {MAT_DATE_LOCALE} from "@angular/material/core";
+import {NgbAccordionDirective, NgbAccordionModule, NgbAlert} from "@ng-bootstrap/ng-bootstrap";
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FullCalendarModule, EnquiryFormComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  providers: [
-    {provide: MAT_DATE_LOCALE, useValue: 'de-DE'}
-  ]
-})
-export class AppComponent {
-  title = 'VBookingBuddy';
-  calendarOptions: CalendarOptions = {
-    initialView: 'multiMonthYear',
-    multiMonthMaxColumns: 1,
-    plugins: [multiMonthPlugin]
-  };
+@Component(
+  {
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+      RouterOutlet,
+      EnquiryFormComponent,
+      NgbAccordionModule,
+      NgbAlert],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    providers: [
+      {provide: MAT_DATE_LOCALE, useValue: 'de-DE'}
+    ]
+  }
+)
+export class AppComponent implements AfterViewInit {
+  @ViewChild('accMenue') accMenue!: NgbAccordionDirective;
+  title = 'VBookingBuddy &#9400; by JK';
+
+  ngAfterViewInit() {
+  }
+
 }
